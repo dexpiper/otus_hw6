acc66222ec729dedd93a0f4c7852334e289bc53f
dexpiper <dexpiper@yandex.ru>
2022-02-08 16:56:07 +0300

    Templates basedir

--- hasker/settings.py
+++ hasker/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'hasker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'hasker/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
