69b7135a3ea5f4ae21860bdf678bc0631c380aab
dexpiper <dexpiper@yandex.ru>
2022-02-27 22:00:07 +0300

    Bool field in users

--- users/forms.py
+++ users/forms.py
@@ -2,25 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-class AlertsBoolField(forms.CharField):
-    def to_python(self, value):
-        print(f'Incoming alerts value: {value}')
-        return True if value == 'on' else False
-
-
 class ProfileForm(forms.Form):
     avatar = forms.ImageField(required=False, label='Upload your avatar')
     email = forms.EmailField(required=False)
-    alerts = AlertsBoolField(required=False)
+    alerts = forms.BooleanField(required=False)
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super(ProfileForm, self).__init__(*args, **kwargs)
-
-    def clean_alerts(self):
-        provided_alerts = self.cleaned_data.get('alerts')
-        self.alerts = provided_alerts
-        return provided_alerts
 
     def clean_email(self):
         provided_email = self.cleaned_data.get('email')
