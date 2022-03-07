d9e91e17fcca93e1c49cba27d98cb356f3466c89
dexpiper <dexpiper@yandex.ru>
2022-03-07 22:44:25 +0300

    Updating statics

--- hasker/urls.py
+++ hasker/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import RedirectView
 
 from questions import views
@@ -22,3 +23,4 @@ urlpatterns = [
            document_root=settings.MEDIA_ROOT) +
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
+urlpatterns += staticfiles_urlpatterns()
