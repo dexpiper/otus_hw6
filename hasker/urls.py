9aed580b77560ac36816cdb73ea4e99848ddf6f6
dexpiper <dexpiper@yandex.ru>
2022-03-07 23:03:42 +0300

    urls static

--- hasker/urls.py
+++ hasker/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
 ] + (
     static(settings.MEDIA_URL,
            document_root=settings.MEDIA_ROOT) +
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT)
 )
 urlpatterns += staticfiles_urlpatterns()
